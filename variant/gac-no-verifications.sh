211d73aa25fa0d606a0834170be662d4ae722050
DevPolo <paul.crussaire@gmail.com>
2021-05-10 19:53:50 +0200

    ✅ NEW FEAT: variant folder with no verifications

---
+++ variant/gac-no-verifications.sh
@@ -0,0 +1,56 @@
+#!/bin/zsh
+
+function gac() {
+  
+  if [ $# -eq 0 ]; then 
+    git add -A && git commit -m "🔧 REFACTOR: no comments"
+    return 1
+  fi  
+
+  if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+    echo "------"
+    echo "Semantic reminder:"
+    echo "📖 DOC:       d"
+    echo "🐛 FIX:       f"
+    echo "👌 IMPROVE:   i"
+    echo "✅ NEW FEAT:  n"
+    echo "🔧 REFACTOR:  null"
+    echo "🚀 RELEASE:   r"
+    echo "🧪 TEST:      t"
+    echo "------"
+    return 1
+  fi  
+
+
+  SEMANTIC=$1
+  shift ;
+  COMMENT=$@
+
+  # Write or edit existing documentation
+  if [ "$SEMANTIC" = "d" ]; then
+    SEMANTIC="📖 DOC:"
+
+  # Fix a bug
+  elif [ "$SEMANTIC" = "f" ]; then
+    SEMANTIC="🐛 FIX:"
+  
+  # Refator your code base
+  elif [ "$SEMANTIC" = "i" ]; then
+    SEMANTIC="👌 IMPROVE:"
+
+  # Add new features
+  elif [ "$SEMANTIC" = "n" ]; then
+    SEMANTIC="✅ NEW FEAT:"
+
+  # Ready for production
+  elif [ "$SEMANTIC" = "r" ]; then 
+    SEMANTIC="🚀 RELEASE:"
+
+  # Test your code
+  elif [ "$SEMANTIC" = "t" ]; then 
+    SEMANTIC="🧪 TEST:"
+  fi
+  
+  git add -A && git commit -m "$SEMANTIC $COMMENT"
+  return 1
+}
