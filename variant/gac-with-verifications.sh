0a334dd84ff35a1b0fec9c659f6d75ac1f25a8a9
DevPolo <paul.crussaire@gmail.com>
2021-05-11 16:14:27 +0200

    🐛 BUG FIX: working on icon

--- variant/gac-with-verifications.sh
+++ variant/gac-with-verifications.sh
@@ -15,7 +15,7 @@ function gac() {
     echo "🪚 REFACTOR:      r" 
     echo "🎨 STYLE:         s"
     echo "🧪 TEST:          t"
-    echo "🛠 WORKING ON:    w"
+    echo "🛠  WORKING ON:    w"
     echo "------"
     return 1
   fi 
@@ -59,7 +59,7 @@ function gac() {
 
   # Working on a feature
   elif [ "$SHORTCUT" = "w" ]; then 
-    SHORTCUT="🛠 WORKING ON:"
+    SHORTCUT="🛠  WORKING ON:"
  
   else
     # ask confirmation if you miss a semantic above
